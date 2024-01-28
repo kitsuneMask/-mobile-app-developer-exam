09a100f50fe882d4f86e67fa0de685fb28b34b0c
RYM <ryeses14@gmail.com>
2024-01-28 23:46:41 +0800

    Final Commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,139 @@
+<!-- TEMPLATE -->
 <template>
+
   <ion-app>
-    <ion-router-outlet />
+
+    <!-- SIDE BAR -->
+    <ion-menu content-id="main-content">
+
+      <!-- CONTENT -->
+      <ion-content class="ion-padding">
+
+        <UserProfile />
+
+        <!-- NAV1 -->
+        <ul>
+
+          <li data-navigation="home" data-path="/home" @click="NAVIGATE" class="nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'home'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/burger_logo.png')]"></span>Home
+            </ion-menu-toggle>
+          </li>
+          <li data-navigation="order-now" data-path="/order-now" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'order-now'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/order_now.svg')] group-[.active]:bg-[url('../img/order_now_active.svg')]"></span>Order Now!
+            </ion-menu-toggle>
+          </li>
+          <li data-navigation="notifications" data-path="/notifications" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'notifications'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/notifications.svg')] group-[.active]:bg-[url('../img/notifications_active.svg')]"></span>Notifications
+            </ion-menu-toggle>
+          </li>
+          <li data-navigation="store-locator" data-path="/store-locator" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'store-locator'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/store_locator.svg')] group-[.active]:bg-[url('../img/store_locator_active.svg')]"></span>Store Locator
+            </ion-menu-toggle>
+          </li>
+          <li data-navigation="faqs" data-path="/faqs" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'faqs'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/FAQs.svg')] group-[.active]:bg-[url('../img/FAQs_active.svg')]"></span>FAQ's
+            </ion-menu-toggle>
+          </li>
+
+        </ul>
+
+        <!-- divider -->
+        <span class="block w-full border-t-2 border-gray-300 my-5"></span>
+
+        <!-- NAV2 -->
+        <ul>
+
+          <li data-navigation="my-orders" data-path="/my-orders" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'my-orders'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/my_orders.svg')] group-[.active]:bg-[url('../img/my_orders_active.svg')]"></span>My Orders
+            </ion-menu-toggle>
+          </li>
+          <li data-navigation="my-account" data-path="/my-account" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'my-account'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/my_account.svg')] group-[.active]:bg-[url('../img/my_account_active.svg')]"></span>My Account
+            </ion-menu-toggle>
+          </li>
+          <li data-navigation="my-favorites" data-path="/my-favorites" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'my-favorites'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/my_favorites.svg')] group-[.active]:bg-[url('../img/my_favorites_active.svg')]"></span>My Favorites
+            </ion-menu-toggle>
+          </li>
+          <li data-navigation="order-tracker" data-path="/order-tracker" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'order-tracker'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/order_tracker.svg')] group-[.active]:bg-[url('../img/order_tracker_active.svg')]"></span>Order Tracker
+            </ion-menu-toggle>
+          </li>
+          <li data-navigation="order-history" data-path="/order-history" @click="NAVIGATE" class="group nav-buttons" :class="{ active : STORE_NAVIGATION.activeNavigation === 'order-history'}">
+            <ion-menu-toggle class="flex items-center">
+                <span class="nav-buttons-icons bg-[url('../img/order_history.svg')] group-[.active]:bg-[url('../img/order_history_active.svg')]"></span>Order History
+            </ion-menu-toggle>
+          </li>
+
+        </ul>
+
+        <!-- logout -->
+        <ion-button color="danger" class="mt-[30px]">Logout</ion-button>
+
+      </ion-content>
+
+    </ion-menu>
+
+    <!-- ACTIVE COMPONENT -->
+    <ion-page id="main-content">
+
+      <ion-router-outlet />
+
+    </ion-page>
+
   </ion-app>
+
 </template>
 
-<script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+
+<!-- SCRIPT -->
+<script setup>
+
+  // IONIC
+  import { 
+    
+    IonApp, 
+    IonRouterOutlet, 
+    IonContent, 
+    IonMenu, 
+    IonMenuToggle,
+    IonPage, 
+    IonButton
+  
+  } from '@ionic/vue';
+
+  // VUE
+  import { reactive, computed, watch } from "vue";
+
+  // VUE ROUTER
+  import { useRouter, useRoute } from "vue-router";
+  const USE_ROUTER = useRouter( );
+  const USE_ROUTE = useRoute( );
+
+  // COMPONENTS
+  import UserProfile from "./components/UserProfile.vue";
+
+  // STORE
+  import storeNavigation from "./pinia/store-navigation.js";
+  const STORE_NAVIGATION = storeNavigation( );
+
+
+  const NAVIGATE = ( ev ) => {
+
+    STORE_NAVIGATION.changeActivePath( ev.currentTarget.dataset.path );
+    STORE_NAVIGATION.changeActiveNavigation( ev.currentTarget.dataset.navigation );
+
+    USE_ROUTER.push( STORE_NAVIGATION.activePath );
+
+  };
+
 </script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,324 @@
+<!-- TEMPLATE -->
 <template>
+
   <ion-page>
-    <ion-header :translucent="true">
+    
+    <!-- HEADER -->
+    <ion-header class="shadow-none">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+
+        <ion-buttons slot="start" class="bg-none">
+          <ion-menu-button>
+            <img src="../img/side_bar.svg" class="scale-150" />
+          </ion-menu-button>
+        </ion-buttons>
+
+        <!-- PROFILE IMAGE, TIER & LOYALTY POINTS -->
+        <div class="flex justify-between">
+
+          <!-- TIER & LOYALTY POINTS -->
+          <div class="flex items-center">
+
+            <img src="../img/crown.webp" class="w-[32px] h-auto object-contain ml-5" >
+
+            <span class="ml-5 font-bold">0 Points</span>
+
+            <ion-icon class="ml-2.5 text-lg text-red-500 font-semibold" name="chevron-forward-outline"></ion-icon>
+
+          </div>
+
+          <img src="../img/Passport Size Formal Attire PH Final.jpg" 
+            
+            class="
+
+                size-[40px]
+                aspect-square
+                rounded-full 
+                border-2
+                border-gray-200 
+                mr-5
+                object-cover 
+                
+            "
+            
+          >
+
+        </div>
+
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <!-- CONTENT -->
+    <ion-content class="ion-padding"> 
+      
+      <!-- LOGO & TAGLINE -->
+      <div class="flex">
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <img src="../img/burger_logo.png" class="w-[90px] aspect-square object-contain" >
+
+        <!-- TAG LINE -->
+        <div class="ml-5">
+
+          <span class="text-xl text-gray-500">Konnichiwa!</span>
+          <h2 class="text-[40px] font-bold">What's the beef?</h2>
+
+        </div>
+
       </div>
+
+      <!-- SEARCH BAR -->
+      <div class="flex items-center h-[60px] rounded-xl border-2 border-gray-200 bg-white mt-5" @click="SEARCHBAR.focus">
+
+        <ion-icon name="search" size="large" color="danger" class="ml-2.5"></ion-icon>
+        <input type="search" placeholder="try our new Mount Burgerest!" class="basis-full border-none outline-none bg-transparent ml-5">
+
+      </div>
+
+      <!-- SPECIAL OFFERS -->
+      <section class="flex flex-col mt-5">
+
+        <!-- HEAD -->
+        <div class="flex justify-between">
+
+          <h3 class="text-xl font-bold">Special Offers</h3>
+
+          <!-- VIEW ALL -->
+          <button class="flex items-center text-red-500">
+
+            View All<ion-icon class="ml-[5px] text-base text-red-500 font-semibold" name="chevron-forward-outline"></ion-icon>
+
+          </button>
+
+        </div>
+
+        <!-- CONTENT -->
+        <div class="flex mt-2.5 overflow-x-scroll">
+
+          <!-- BURGERS -->
+          <ul class="flex flex-nowrap gap-x-5">
+
+            <!-- BURGER -->
+            <li 
+
+              v-for="burger in STORE_BURGERS.specialOffers"
+            
+              class="
+
+                flex
+                flex-col
+                w-[300px] 
+                h-[150px] 
+                bg-[url('../img/special_offers.svg')]
+                bg-no-repeat
+                bg-cotain
+                bg-center
+                relative
+                
+              "
+            >
+
+              <!-- offer -->
+              <span class="text-xl text-red-500 font-bold ml-5 mt-2.5">20% OFF!!!</span>
+
+              <!-- image -->
+              <img :src="burger.images[ 0 ].sm" class="w-[80px] h-auto ml-5">
+
+              <!-- NAME -->
+              <div class="w-[200px] absolute top-[30px] left-[100px]">
+
+                <span class="text-2xl text-gray-100 font-bold leading-5 uppercase absolute opacity-50">{{ burger.name }}</span>
+                <span class="text-2xl text-gray-500 font-bold leading-5 uppercase absolute translate-x-[5px] translate-y-[5px] opacity-50">{{ burger.name }}</span>
+                <span class="text-2xl font-bold leading-5 uppercase absolute translate-x-[10px] translate-y-[10px]">{{ burger.name }}</span>
+
+              </div>
+
+              <!-- PRICE -->
+              <div class="flex flex-col items-end absolute right-2.5 bottom-2.5">
+
+                <span class="text-white">${{ burger.price }}</span>
+                <span class="text-xl text-white font-bold leading-5">${{ ( burger.price * .80 ).toFixed( 2 ) }}</span>
+
+                <!-- price cut -->
+                <img src="../img/special_offers_price_cut.svg" class="absolute top-[10px] right-0">
+
+              </div>
+
+            </li>
+            
+          </ul>
+
+        </div>
+
+      </section>
+
+      <!-- CATEGORY -->
+      <section class="flex flex-col mt-5">
+
+        <h3 class="text-xl font-bold">Category</h3>
+
+        <!-- CONTENT -->
+        <div class="flex flex-nowrap gap-2.5 mt-2.5 overflow-x-scroll">
+
+          <!-- AFFORDABLE -->
+          <div class="flex flex-col justify-end min-w-[200px] h-[300px] border-2 border-gray-200 rounded-2xl bg-[url('../img/Affordable_Burgers.png')] bg-no-repeat relative">
+
+            <!-- cover -->
+            <span class="w-full h-full rounded-2xl bg-black absolute top-0 left-0 opacity-10"></span>
+
+            <h3 class="ml-2.5 text-2xl text-red-500 font-bold leading-[26px] drop-shadow-lg relative">AFFORDABLE</h3>
+            <span class="ml-2.5 mb-2.5 text-lg text-white leading-5 relative">{{ STORE_BURGERS.affordable.length }} Burgers Available</span>
+
+          </div>
+
+          <!-- LUXURIOUS -->
+          <div class="flex flex-col justify-end min-w-[200px] h-[300px] border-2 border-gray-200 rounded-2xl bg-[url('../img/Luxurious_Burgers.png')] bg-no-repeat relative">
+
+            <!-- cover -->
+            <span class="w-full h-full rounded-2xl bg-black absolute top-0 left-0 opacity-10"></span>
+
+            <h3 class="ml-2.5 text-2xl text-red-500 font-bold leading-[26px] drop-shadow-lg relative">LUXURIOUS</h3>
+            <span class="ml-2.5 mb-2.5 text-lg text-white leading-5 relative">{{ STORE_BURGERS.luxurious.length }} Burgers Available</span>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      <!-- CATEGORY -->
+      <section class="flex flex-col mt-5">
+
+        <h3 class="text-xl font-bold">What's New?</h3>
+
+        <!-- CONTENT -->
+        <div class="flex flex-nowrap gap-2.5 mt-2.5 overflow-x-scroll">
+
+          <div class="min-w-[300px] h-[150px] border-2 border-gray-200 rounded-xl bg-[url('../img/Mount_Burgerest.png')] bg-no-repeat bg-center bg-cover"></div>
+
+          <div class="min-w-[300px] h-[150px] border-2 border-gray-200 rounded-xl bg-[url('../img/Monsta_Burger.png')] bg-no-repeat bg-center bg-cover"></div>
+
+        </div>
+
+      </section>
+
+      <!-- CATEGORY -->
+      <section class="flex flex-col mt-5 mb-10">
+
+        <h3 class="text-xl font-bold">Most Popular</h3>
+
+        <!-- CONTENT -->
+        <div class="flex mt-2.5">
+
+          <ul class="flex flex-wrap justify-center gap-[5px]">
+
+            <li
+
+              v-for="burger in STORE_BURGERS.popular"
+
+              class="
+              
+                flex
+                flex-col
+                min-w-[160px]
+                max-w-[150px]
+                h-[250px]
+                max
+                p-[5px]
+                border-2
+                border-gray-200
+                rounded-xl
+
+              "
+            >
+
+              <img :src="burger.images[ 0 ].sm" class="self-center w-[120px]">
+
+              <!-- name -->
+              <h4 class="max-h-10 mt-5 text-base font-bold leading-5 overflow-hidden">{{ burger.name }}</h4>
+
+              <!-- category -->
+              <span class="mt-[5px] text-sm text-red-500 leading-4">{{ burger.price > 9 ? "LUXURIOUS" : "AFFORDABLE" }}</span>
+
+              <!-- PRICE & RATING -->
+              <div class="flex justify-between mt-2.5">
+
+                <span class="text-lg text-red-500 font-bold">${{ burger.price.toFixed( 2 ) }}</span>
+
+                <img src="../img/star_rating.png" class="w-[70px] h-auto object-contain">
+
+              </div>
+          
+            </li>
+
+          </ul>
+
+        </div>
+
+      </section>
+
+      <!-- order now -->
+      <button data-navigation="order-now" data-path="/order-now" @click="NAVIGATE" class="rounded-lg py-[15px] px-10 bg-red-500/95 text-white shadow-lg fixed right-5 bottom-5">Order Now!</button>
+
     </ion-content>
+
   </ion-page>
+
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+<!-- SCRIPT -->
+<script setup>
+
+  // IONIC VUE
+  import { 
+    
+    IonPage,
+    IonContent, 
+    IonHeader, 
+    IonIcon, 
+    IonToolbar,
+    IonButtons,
+    IonMenuButton,
+  
+  } from '@ionic/vue';
+
+  // VUE ROUTER
+  import { useRouter } from "vue-router";
+  const USE_ROUTER = useRouter( );
+
+  // STORE
+  import storeNavigation from "../pinia/store-navigation.js";
+  import storeBurgers from "../pinia/store-burgers.js";
+  const STORE_NAVIGATION = storeNavigation( );
+  const STORE_BURGERS = storeBurgers( );
+
+  // NAVIGATE
+  const NAVIGATE = ( ev ) => {
+
+    STORE_NAVIGATION.changeActivePath( ev.currentTarget.dataset.path );
+    STORE_NAVIGATION.changeActiveNavigation( ev.currentTarget.dataset.navigation );
+
+    USE_ROUTER.push( STORE_NAVIGATION.activePath );
+
+  };
+
+  // SEARCHBAR
+  const SEARCHBAR = {
+
+    focus ( ) {
+
+      document.querySelector( "input[type='search']" ).focus( );
+
+    }
+
+  };
+
 </script>
 
+
+<!-- STYLE -->
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
+ 
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
