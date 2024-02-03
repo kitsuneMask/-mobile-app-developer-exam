13971669290d46b81c13bb40b702d0a7e8404b06
RYM <ryeses14@gmail.com>
2024-02-03 15:05:58 +0800

    Fixed Loyalty Points navigation indicator

--- src/components/ProfileSideBar.vue
+++ src/components/ProfileSideBar.vue
@@ -23,11 +23,15 @@
             >
 
             <!-- TIER & LOYALTY POINTS -->
-            <div @click="USE_ROUTER.push( '/loyalty-points' )" class="flex items-center">
+            <div data-navigation="loyalty-points" data-path="/loyalty-points" @click="NAVIGATE" class="flex items-center">
 
                 <img src="../img/crown.webp" class="w-[32px] h-auto object-contain ml-5" />
 
-                <span class="ml-5 font-bold">0 Points</span>
+                <ion-menu-toggle class="group" :class="{ active : STORE_NAVIGATION.activeNavigation === 'loyalty-points' }">
+
+                    <span class="ml-5 group-[.active]:text-red-500 font-bold">0 Points</span>
+
+                </ion-menu-toggle>
 
                 <img src="../img/chevron_right.svg" />
 
@@ -49,8 +53,25 @@
 <!-- SCRIPT -->
 <script setup>
 
+    // IONIC
+    import { IonMenuToggle } from "@ionic/vue";
+
     // VUE ROUTER
     import { useRouter } from 'vue-router';
     const USE_ROUTER = useRouter( );
+
+    // STORE
+    import storeNavigation from '../pinia/store-navigation';
+    const STORE_NAVIGATION = storeNavigation( );
+
+
+    const NAVIGATE = ( ev ) => {
+
+        STORE_NAVIGATION.changeActivePath( ev.currentTarget.dataset.path );
+        STORE_NAVIGATION.changeActiveNavigation( ev.currentTarget.dataset.navigation );
+
+        USE_ROUTER.push( STORE_NAVIGATION.activePath );
+
+    };
 
 </script>
